5ac563afa5da55b74c1230c6cbbe937c8cf5f7e8
Patrick Meier <pmeier747@hotmail.co.uk>
2024-06-02 15:04:19 +0100

    Initial Test Version

---
+++ js/output.js
@@ -0,0 +1,137 @@
+"use strict";
+class EventDetails {
+    ShowEvent = false;
+    Title = "";
+    ShortDescription = "";
+    FullDescription = "";
+    Town = "";
+    Location = "";
+    ShowDate = new Date();
+    DoorTime = null;
+    StartTime = null;
+    EndTime = null;
+    TicketLink = null;
+}
+document.addEventListener("DOMContentLoaded", InitPage);
+function InitPage() {
+    let excelLocationCSV = new URL("https://docs.google.com/spreadsheets/d/e/2PACX-1vRBjp_krL1yLMOpXHTfLsBMqD85ivI_aguisYMGJAk4ctP2fn2bSHobbjbZ3TEi2qs7BaxwaHuIQnEG/pub?output=csv");
+    let localFile = new URL("/data/TestData.csv", window.origin);
+    let fileURL = window.origin.indexOf("localhost") == -1 ? excelLocationCSV : localFile;
+    let eventDataClient = new XMLHttpRequest();
+    eventDataClient.onerror = function (e) { console.log(this, e, "Error"); };
+    eventDataClient.onloadend = DataLoaded;
+    eventDataClient.open("GET", localFile);
+    eventDataClient.send();
+}
+function DataLoaded(e) {
+    console.log(this);
+    let fileContent = this.responseText;
+    let firstNewLine = fileContent.indexOf("\r\n");
+    let header = fileContent.substring(0, firstNewLine);
+    let headerItems = header.split(",");
+    let body = fileContent.substring(firstNewLine + 2);
+    let bodyItems = body.split("\r\n");
+    let formattedBodyItems = [];
+    for (let i = 0; i < bodyItems.length; i++) {
+        let individualItems = bodyItems[i].split(",");
+        let finalItems = [];
+        let itemToAdd = "";
+        let inSubString = false;
+        for (let j = 0; j < individualItems.length; j++) {
+            let currentItem = individualItems[j];
+            if (!inSubString) {
+                itemToAdd = currentItem;
+            }
+            else {
+                itemToAdd += currentItem;
+                if (currentItem.charAt(currentItem.length - 1) == "\"") {
+                    inSubString = false;
+                }
+                else {
+                    continue;
+                }
+            }
+            if (currentItem.charAt(0) == "\"") {
+                inSubString = true;
+                continue;
+            }
+            finalItems.push(itemToAdd);
+            itemToAdd = "";
+            inSubString = false;
+        }
+        formattedBodyItems.push(finalItems);
+    }
+    let showEventIndex = headerItems.indexOf("Show Event");
+    let titleIndex = headerItems.indexOf("Title");
+    let shortDescriptionIndex = headerItems.indexOf("Short Description");
+    let fullDescriptionIndex = headerItems.indexOf("Full Description");
+    let townIndex = headerItems.indexOf("Town");
+    let locationIndex = headerItems.indexOf("Location");
+    let dateIndex = headerItems.indexOf("Date");
+    let doorTimeIndex = headerItems.indexOf("Door Time");
+    let startTimeIndex = headerItems.indexOf("Start Time");
+    let endTimeIndex = headerItems.indexOf("End Time");
+    let ticketLinkIndex = headerItems.indexOf("Ticket Link");
+    let allEvent = [];
+    for (let i = 0; i < formattedBodyItems.length; i++) {
+        let dataItem = formattedBodyItems[i];
+        let eventItem = new EventDetails();
+        eventItem.ShowEvent = dataItem[showEventIndex] == "TRUE";
+        eventItem.Title = dataItem[titleIndex];
+        eventItem.ShortDescription = dataItem[shortDescriptionIndex];
+        eventItem.FullDescription = dataItem[fullDescriptionIndex];
+        eventItem.Town = dataItem[townIndex];
+        eventItem.Location = dataItem[locationIndex];
+        eventItem.ShowDate = parseDate(dataItem[dateIndex]);
+        eventItem.DoorTime = parseTime(dataItem[doorTimeIndex], eventItem.ShowDate);
+        eventItem.StartTime = parseTime(dataItem[startTimeIndex], eventItem.ShowDate);
+        eventItem.EndTime = parseTime(dataItem[endTimeIndex], eventItem.ShowDate);
+        try {
+            eventItem.TicketLink = new URL(dataItem[ticketLinkIndex]);
+        }
+        catch {
+            eventItem.TicketLink = null;
+        }
+        allEvent.push(eventItem);
+    }
+    createTable(allEvent);
+}
+function parseDate(dateString) {
+    let splitDates = dateString.split("/");
+    let day = parseInt(splitDates[0]);
+    let month = parseInt(splitDates[1]);
+    let year = parseInt(splitDates[2]);
+    return new Date(year, month, day);
+}
+function parseTime(timeString, baseDate) {
+    let splitTimes = timeString.split(":");
+    let hour = parseInt(splitTimes[0]);
+    let minute = parseInt(splitTimes[1]);
+    return new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDay(), hour, minute);
+}
+function createTable(events) {
+    let eventsContainer = document.getElementById("eventsContainer");
+    if (eventsContainer == null) {
+        console.log("No container");
+        return;
+    }
+    for (let i = 0; i < events.length; i++) {
+        let event = events[i];
+        if (event.ShowEvent == false) {
+            continue;
+        }
+        let elem = document.createElement("div");
+        let titleText = document.createElement("p");
+        titleText.textContent = event.Title;
+        let descriptionText = document.createElement("p");
+        descriptionText.innerHTML = event.FullDescription.replaceAll("\n", "<br/>");
+        let dateText = document.createElement("p");
+        dateText.textContent = event.ShowDate.toDateString();
+        elem.append(titleText);
+        elem.append(descriptionText);
+        elem.append(dateText);
+        elem.append(document.createElement("hr"));
+        eventsContainer.append(elem);
+    }
+    let elem = document.createElement("div");
+}
---
+++ ts/EventDataImport.ts
@@ -0,0 +1,155 @@
+class EventDetails {
+    public ShowEvent: boolean = false;
+    public Title: string = "";
+    public ShortDescription: string = "";
+    public FullDescription: string = "";
+    public Town: string = "";
+    public Location: string = "";
+    public ShowDate: Date = new Date();
+    public DoorTime: Date | null = null;
+    public StartTime: Date | null = null;
+    public EndTime: Date | null = null;
+    public TicketLink: URL | null = null;
+}
+
+document.addEventListener("DOMContentLoaded", InitPage);
+
+function InitPage(): void {
+    let excelLocationCSV: URL = new URL("https://docs.google.com/spreadsheets/d/e/2PACX-1vRBjp_krL1yLMOpXHTfLsBMqD85ivI_aguisYMGJAk4ctP2fn2bSHobbjbZ3TEi2qs7BaxwaHuIQnEG/pub?output=csv");
+    let localFile: URL = new URL("/data/TestData.csv", window.origin);
+    let fileURL: URL = window.origin.indexOf("localhost") == -1 ? excelLocationCSV : localFile;
+
+    let eventDataClient: XMLHttpRequest = new XMLHttpRequest();
+    eventDataClient.onerror = function (this: XMLHttpRequest, e: ProgressEvent<EventTarget>) { console.log(this, e, "Error"); }
+    eventDataClient.onloadend = DataLoaded;
+    eventDataClient.open("GET", localFile);
+    eventDataClient.send();
+}
+
+function DataLoaded(this: XMLHttpRequest, e: ProgressEvent<EventTarget>) {
+    console.log(this);
+
+    let fileContent: string = this.responseText;
+    let firstNewLine: number = fileContent.indexOf("\r\n");
+    let header: string = fileContent.substring(0, firstNewLine);
+
+    let headerItems: string[] = header.split(",");
+
+    let body: string = fileContent.substring(firstNewLine + 2);
+    let bodyItems: string[] = body.split("\r\n");
+
+    let formattedBodyItems: string[][] = [];
+    for (let i: number = 0; i < bodyItems.length; i++) {
+        let individualItems: string[] = bodyItems[i].split(",");
+        let finalItems: string[] = [];
+        let itemToAdd: string = "";
+        let inSubString: boolean = false;
+        for (let j = 0; j < individualItems.length; j++) {
+            let currentItem: string = individualItems[j];
+            if (!inSubString) {
+                itemToAdd = currentItem;
+            }
+            else {
+                itemToAdd += currentItem;
+                if (currentItem.charAt(currentItem.length - 1) == "\"") {
+                    inSubString = false;
+                }
+                else {
+                    continue;
+                }
+            }
+
+            if (currentItem.charAt(0) == "\"") {
+                inSubString = true;
+                continue;
+            }
+
+            finalItems.push(itemToAdd);
+            itemToAdd = "";
+            inSubString = false;
+        }
+        formattedBodyItems.push(finalItems);
+    }
+
+    let showEventIndex: number = headerItems.indexOf("Show Event");
+    let titleIndex: number = headerItems.indexOf("Title");
+    let shortDescriptionIndex: number = headerItems.indexOf("Short Description");
+    let fullDescriptionIndex: number = headerItems.indexOf("Full Description");
+    let townIndex: number = headerItems.indexOf("Town");
+    let locationIndex: number = headerItems.indexOf("Location");
+    let dateIndex: number = headerItems.indexOf("Date");
+    let doorTimeIndex: number = headerItems.indexOf("Door Time");
+    let startTimeIndex: number = headerItems.indexOf("Start Time");
+    let endTimeIndex: number = headerItems.indexOf("End Time");
+    let ticketLinkIndex: number = headerItems.indexOf("Ticket Link");
+
+    let allEvent: EventDetails[] = []
+    for (let i: number = 0; i < formattedBodyItems.length; i++) {
+        let dataItem: string[] = formattedBodyItems[i];
+        let eventItem: EventDetails = new EventDetails();
+        eventItem.ShowEvent = dataItem[showEventIndex] == "TRUE";
+        eventItem.Title = dataItem[titleIndex];
+        eventItem.ShortDescription = dataItem[shortDescriptionIndex];
+        eventItem.FullDescription = dataItem[fullDescriptionIndex];
+        eventItem.Town = dataItem[townIndex];
+        eventItem.Location = dataItem[locationIndex];
+        eventItem.ShowDate = parseDate(dataItem[dateIndex]);
+        eventItem.DoorTime = parseTime(dataItem[doorTimeIndex], eventItem.ShowDate);
+        eventItem.StartTime = parseTime(dataItem[startTimeIndex], eventItem.ShowDate);
+        eventItem.EndTime = parseTime(dataItem[endTimeIndex], eventItem.ShowDate);
+        try {
+            eventItem.TicketLink = new URL(dataItem[ticketLinkIndex]);
+        }
+        catch {
+            eventItem.TicketLink = null;
+        }
+
+        allEvent.push(eventItem);
+    }
+
+    createTable(allEvent);
+}
+
+function parseDate(dateString: string): Date {
+    let splitDates: string[] = dateString.split("/");
+    let day: number = parseInt(splitDates[0]);
+    let month: number = parseInt(splitDates[1]);
+    let year: number = parseInt(splitDates[2]);
+
+    return new Date(year, month, day);
+}
+
+function parseTime(timeString: string, baseDate: Date): Date {
+    let splitTimes: string[] = timeString.split(":");
+    let hour: number = parseInt(splitTimes[0]);
+    let minute: number = parseInt(splitTimes[1]);
+
+    return new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDay(), hour, minute);
+}
+
+function createTable(events: EventDetails[]): void {
+    let eventsContainer: HTMLDivElement | null = document.getElementById("eventsContainer") as HTMLDivElement;
+    if (eventsContainer == null) {
+        console.log("No container");
+        return;
+    }
+
+    for (let i: number = 0; i < events.length; i++) {
+        let event: EventDetails = events[i];
+        if (event.ShowEvent == false) { continue; }
+
+        let elem: HTMLDivElement = document.createElement("div");
+        let titleText: HTMLParagraphElement = document.createElement("p");
+        titleText.textContent = event.Title;
+        let descriptionText: HTMLParagraphElement = document.createElement("p");
+        descriptionText.innerHTML = event.FullDescription.replaceAll("\n", "<br/>");
+        let dateText: HTMLParagraphElement = document.createElement("p");
+        dateText.textContent = event.ShowDate.toDateString();
+        elem.append(titleText);
+        elem.append(descriptionText);
+        elem.append(dateText);
+        elem.append(document.createElement("hr"));
+        eventsContainer.append(elem);
+    }
+    let elem: HTMLDivElement = document.createElement("div");
+}
